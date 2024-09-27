0d6f41757b1cde32a8c9c029307f21bccd051e1b
PanJi-0 <163510072+PanJi-0@users.noreply.github.com>
2024-09-27 09:56:17 +0800

    feat: populate package namespace

--- src/pycounts_pjlearn/__init__.py
+++ src/pycounts_pjlearn/__init__.py
@@ -1,3 +1,7 @@
 # read version from installed package
 from importlib.metadata import version
 __version__ = version("pycounts_pjlearn")
+
+# populate package namespace
+from pycounts_pjlearn.pycounts_pjlearn import count_words
+from pycounts_pjlearn.plotting import plot_words
